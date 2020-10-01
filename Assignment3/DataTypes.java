aa37bedf77df02ca8dea18bab3940ba207e0b621
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-10-01 01:57:14 -0400

    Add files via upload

---
+++ Assignment3/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+  public static void main(String[] args) {
+
+    //primitive data type example
+    //notice the use of lowerCamelCase on the variables
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 3.1415f;    // floating point number
+    char myLetter = 'E';         // character
+    boolean myBool = true;       // boolean
+
+    //non-primitive data type example
+    String myText = "Hello";     // String
+
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
