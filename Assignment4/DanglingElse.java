7750a3c6b81e01303ca7880f53b70853f87c798d
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-10-06 19:50:26 -0400

    Update and rename DanglingIf.java to DanglingElse.java

--- Assignment4/DanglingElse.java
+++ Assignment4/DanglingElse.java
@@ -1,9 +1,9 @@
 //Statement 2
-public class DanglingIf {
+public class DanglingElse {
   public static void main(String[] args) {
     int myNum = 5;        
     if ( myNum > 0 )   
-      if ( myNum < 10 ) 
+      if ( myNum > 10 ) 
         System.out.println( "greater than 10" ) ;
       else //else is indented with the inner if statement from line 6
         System.out.println( "less than 10" ) ;
@@ -13,14 +13,30 @@ public class DanglingIf {
 
 
 //Statement 2
-public class DanglingIf {
+public class DanglingElse {
   public static void main(String[] args) {
     int myNum = 5;        
     if ( myNum > 0 )   
-      if ( myNum < 10 ) 
+      if ( myNum > 10 ) 
         System.out.println( "greater than 10" ) ;
     else //else is indented with the outer if statement from line 20
       System.out.println( "less than 10" ) ;
       //because of the dangling else problem, the output would always be "greater than 10" regardless of the value of myNum as long as it is greater than 0
+    }
+}
+
+//Solution
+public class DanglingIf {
+  public static void main(String[] args) {
+
+    int myNum = 2;        
+    if ( myNum > 5 )  { 
+      if ( myNum > 10 ) {
+        System.out.println( "greater than 10" );
+      }
+    }
+    else{
+        System.out.println( "less than 10" );
+      }
     }
 }
