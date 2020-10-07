f140608af3a6fe521dcfac246b7950676c746118
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-10-07 03:53:21 -0400

    Update DanglingElse.java

--- Assignment4/selection structure/DanglingElse.java
+++ Assignment4/selection structure/DanglingElse.java
@@ -1,13 +1,16 @@
-//Statement 2
+//Statement 1
 public class DanglingElse {
   public static void main(String[] args) {
-    int myNum = 5;        
-    if ( myNum > 0 )   
-      if ( myNum > 10 ) 
-        System.out.println( "greater than 10" ) ;
-      else //else is indented with the inner if statement from line 6
-        System.out.println( "less than 10" ) ;
-      //because of the dangling else problem, the output would always be "greater than 10" regardless of the value of myNum as long as it is greater than 0
+  int x=4;
+  int y=12;
+
+  if(x>5){
+      if(y>10)
+        System.out.println("First case");
+      }
+      else //else is indented with the inner if statement from line 8
+        System.out.println("Second case");
+      //because of the dangling else problem,Java would either print the first case or nothing at all
     }
 }
 
@@ -15,28 +18,31 @@ public class DanglingElse {
 //Statement 2
 public class DanglingElse {
   public static void main(String[] args) {
-    int myNum = 5;        
-    if ( myNum > 0 )   
-      if ( myNum > 10 ) 
-        System.out.println( "greater than 10" ) ;
-    else //else is indented with the outer if statement from line 20
-      System.out.println( "less than 10" ) ;
-      //because of the dangling else problem, the output would always be "greater than 10" regardless of the value of myNum as long as it is greater than 0
+  int x=4;
+  int y=12;
+
+  if(x>5){
+      if(y>10)
+        System.out.println("First case");
+      }
+  else //else is indented with the outer if statement from line 24
+      System.out.println("Second case");
+    //The Java virtual machine does not care about indentation
     }
 }
 
 //Solution
 public class DanglingIf {
   public static void main(String[] args) {
+  int x=4;
+  int y=12;
 
-    int myNum = 2;        
-    if ( myNum > 5 )  { 
-      if ( myNum > 10 ) {
-        System.out.println( "greater than 10" );
+  if(x>5){
+      if(y>10)
+      System.out.println("First case");
       }
-    }
-    else{
-        System.out.println( "less than 10" );
-      }
+  else
+  System.out.println("Second case");
+  //The Java can now execute both statements now if the conditions are right!
     }
 }
