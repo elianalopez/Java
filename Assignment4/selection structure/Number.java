96d6f3da1829f7e02aeecf04aff046d0c80723a9
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-10-07 02:12:53 -0400

    Create Number.java

---
+++ Assignment4/selection structure/Number.java
@@ -0,0 +1,17 @@
+public class Number {
+
+    public static void main(String[] args) {   
+
+        int number = 0;
+        //example of an if statement
+        // checks if number is greater than 0	 
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+        //since if is false the else statement would be executed
+        //By default 0 or negative
+        else {
+            System.out.println("The number is 0 or negative");
+        } 
+    }
+}
