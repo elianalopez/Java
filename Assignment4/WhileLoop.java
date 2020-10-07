f7b90dc36103425730252389b2b512f1b79f0db0
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-10-06 20:43:55 -0400

    Add files via upload

---
+++ Assignment4/WhileLoop.java
@@ -0,0 +1,15 @@
+//Using While Loop to find the sum of numbers from 1 to 10
+
+public class WhileLoop { 
+    public static void main(String args[]) 
+    { 
+        int x = 1, sum = 0; 
+  
+        // Exit when x becomes greater than 4 
+        while (x <= 10) { 
+            sum = sum + x; // Total sum of x 
+            x++; // Icrementing the value of x for the next interation
+        } 
+        System.out.println("Sum of numbers from 1 to 10 is: " + sum); 
+    } 
+}
