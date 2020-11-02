9f885b2998dd2f0ae679b60b5a227fa82f182ed2
elianalopez <39322110+elianalopez@users.noreply.github.com>
2020-11-01 19:13:02 -0500

    Create Loop.java

---
+++ Assignment6/Loop.java
@@ -0,0 +1,11 @@
+public class Loop
+{
+    public static void main(String args[]) 
+    { 
+        int x = 1;
+        for (int i =0; i <= 4; i++) {
+            // int x = 3; //This line would cause an error
+            System.out.println(x); 
+        }
+    } 
+}
